ce79318a2381f094d447d977ebf3580708b05b76
sronlemos <sron.lemos@gmail.com>
2022-05-07 09:13:49 -0300

    start project

---
+++ limits-service/src/main/java/com/github/sronlemos/limitsservice/LimitsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.github.sronlemos.limitsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LimitsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LimitsServiceApplication.class, args);
+	}
+
+}
