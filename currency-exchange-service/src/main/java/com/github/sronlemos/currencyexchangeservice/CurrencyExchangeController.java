80f7e188c04a91beca8a0a7d102c6f377f2b913f
sronlemos <sron.lemos@gmail.com>
2022-05-17 22:30:33 -0300

    added jpa

--- currency-exchange-service/src/main/java/com/github/sronlemos/currencyexchangeservice/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/com/github/sronlemos/currencyexchangeservice/CurrencyExchangeController.java
@@ -1,7 +1,5 @@
 package com.github.sronlemos.currencyexchangeservice;
 
-import java.math.BigDecimal;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,11 +10,18 @@ import org.springframework.web.bind.annotation.RestController;
 public class CurrencyExchangeController {
 
 	@Autowired
+	private CurrencyExchangeRepository repository;
+
+	@Autowired
 	private Environment environment;
 
 	@GetMapping("/currency-exchange/from/{from}/to/{to}")
 	public CurrencyExchange retrieveExchangeValue(@PathVariable String from, @PathVariable String to) {
-		CurrencyExchange currencyExchange = new CurrencyExchange(1000L, from, to, BigDecimal.valueOf(50));
+		CurrencyExchange currencyExchange = repository.findByFromAndTo(from, to);
+		if(currencyExchange == null) {
+			throw new RuntimeException("Unable to find for " + from + " to "+ to);
+		}
+		
 		String port = environment.getProperty("local.server.port");
 		currencyExchange.setEnvironment(port);
 		return currencyExchange;
