27c9a63748adcff34a9468d4566f3fad247f8f6e
sronlemos <sron.lemos@gmail.com>
2022-05-18 00:05:34 -0300

    added feign

--- currency-conversion-service/src/main/java/com/github/sronlemos/currencyconversionservice/CurrencyConversionController.java
+++ currency-conversion-service/src/main/java/com/github/sronlemos/currencyconversionservice/CurrencyConversionController.java
@@ -3,6 +3,7 @@ package com.github.sronlemos.currencyconversionservice;
 import java.math.BigDecimal;
 import java.util.HashMap;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,6 +13,26 @@ import org.springframework.web.client.RestTemplate;
 @RestController
 public class CurrencyConversionController {
 
+	@Autowired
+	private CurrencyExchangeProxy proxy;
+	
+	@GetMapping("/currency-conversion-feign/from/{from}/to/{to}/quantity/{quantity}")
+	public CurrencyConversion calculateCurrencyConversionFeign(
+			@PathVariable String from,
+			@PathVariable String to,
+			@PathVariable BigDecimal quantity
+			) {
+		
+		CurrencyConversion currencyConversion = proxy.retrieveExchangeValue(from, to);
+		
+		return new CurrencyConversion(currencyConversion.getId(), 
+				from, to, quantity, 
+				currencyConversion.getConversionMultiple(), 
+				quantity.multiply(currencyConversion.getConversionMultiple()), 
+				currencyConversion.getEnvironment()+ " " + "rest feign");
+		
+	}
+	
 	@GetMapping("/currency-conversion/from/{from}/to/{to}/quantity/{quantity}")
 	public CurrencyConversion calculateCurrencyConversion(
 			@PathVariable String from,
---
+++ currency-conversion-service/src/main/java/com/github/sronlemos/currencyconversionservice/CurrencyExchangeProxy.java
@@ -0,0 +1,13 @@
+package com.github.sronlemos.currencyconversionservice;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(name="curency-exchange", url="localhost:8001")
+public interface CurrencyExchangeProxy {
+	
+	@GetMapping("/currency-exchange/from/{from}/to/{to}")
+	CurrencyConversion retrieveExchangeValue(@PathVariable String from, @PathVariable String to);
+
+}
